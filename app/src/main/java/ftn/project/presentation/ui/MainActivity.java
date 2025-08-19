b149cc23796403b7448149a8934f6953aba34857
LazarMitar <laki26062002@gmail.com>
2025-08-19 13:08:32 +0200

    form for new task

--- app/src/main/java/ftn/project/presentation/ui/MainActivity.java
+++ app/src/main/java/ftn/project/presentation/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package ftn.project;
+package ftn.project.presentation.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import ftn.project.R;
 
 public class MainActivity extends AppCompatActivity {
 
